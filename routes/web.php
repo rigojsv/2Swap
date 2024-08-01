2ba1b7406fae143e46877a3d54a3d88e19b3c585
rigojsv <rigojsv.rsv@gmail.com>
2024-07-31 22:20:43 -0600

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -33,50 +33,33 @@ Route::middleware([
     'verified',
 ])->group(function () {
 
+    //Carrito
     Route::post('/cart/add/{productId}', [CartController::class, 'addProduct'])->name('cart.add');
-
     Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
-
     Route::post('/cart/update/{itemId}', [CartController::class, 'updateCartItem'])->name('cart.update');
-
     Route::delete('/cart/remove/{itemId}', [CartController::class, 'removeCartItem'])->name('cart.remove');
 
-    Route::get('/newproduct', [ProductController::class, 'create'])->name('product.create');
 
-    Route::post('/newproduct', [ProductController::class, 'store'])->name('product.store');
 
-    Route::post('/paymentcart', [ProductController::class, 'cartPayment'])->name('cart.payment');
-
+    //Dashboard
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 
     Route::get('/publications', [ProductController::class, 'myPublications'])->name('publications');
-
     Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
-
     Route::get('/sells', function () {
         return view('sells');
     })->name('mysells');
 
-    Route::get('/newproduct', function () {
-        return view('newproduct');
-    });
-    
-    Route::middleware([
-        'auth:sanctum',
-        config('jetstream.auth_session'),
-        'verified',
-    ])->group(function () {
-        Route::get('/newproduct', [ProductController::class, 'create'])->name('product.create');
-        Route::post('/newproduct', [ProductController::class, 'store'])->name('product.store');
-        // Ruta para la pÃ¡gina de pago
-    Route::get('/pago', [ProductController::class, 'showPaymentPage'])->name('payment.page');
-    
-    // Ruta para procesar el pago
-    Route::post('/pago', [ProductController::class, 'processPayment'])->name('payment.process');
-    Route::get('/payment', [PaymentController::class, 'showPaymentPage'])->name('payment.page');
+
+    //Pagos del carrito
+    Route::get('/paymentcart', [PaymentController::class, 'showPaymentPage'])->name('payment.cart');
     Route::post('/payment', [PaymentController::class, 'processPayment'])->name('payment.process');
-    
-    });
+    Route::post('/paymentcart', [ProductController::class, 'cartPayment'])->name('cart.payment');
+
+    //agregar producto y cobro del mismo
+    Route::get('/newproduct', [ProductController::class, 'create'])->name('product.create');
+    Route::post('/newproduct', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/pago', [ProductController::class, 'showPaymentPage'])->name('payment.page');
 });

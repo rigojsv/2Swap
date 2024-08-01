53d13a87c0ebf9e9683329c4d85c5a7fc0333595
LuisMz04 <157434146+LuisMz04@users.noreply.github.com>
2024-07-31 22:00:10 -0600

    Update cart.blade.php

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -30,7 +30,7 @@
                     <p class="texto">Nombre</p>
                   </div>
                   <div class="col-md-3 col-lg-3 col-xl-2 d-flex border-end border-black">
-                    <p class="texto">Descripcion</p>
+                    <p class="texto">Descripción</p>
                   </div>
                   <div class="col-md-3 col-lg-2 col-xl-2 border-end border-black">
                     <p class="texto">Precio</p>
@@ -41,80 +41,72 @@
                 </div>
               </div>
             </div>
-            <?php
-            $total = 0;
-            ?>
+            
+            <?php $total = 0; ?>
+            
             @if($cart && $cart->items->count() > 0)
-            @foreach($cart->items as $item)
-            <div class="card rounded-3 mb-4">
-              <div class="card-body p-4">
-                <div class="row d-flex justify-content-between align-items-center">
-                  <div class="col-md-2 col-lg-2 col-xl-2">
-                    @if($item->product->images)
-                    @php
-                    $images = json_decode($item->product->images, true); // Decodifica el JSON
-                    @endphp
-                    @if(!empty($images) && is_array($images))
-                    <img src="{{ $images[0] }}" class="img-fluid rounded-3" alt="{{ $item->product->name }}">
+              @foreach($cart->items as $item)
+                <div class="card rounded-3 mb-4">
+                  <div class="card-body p-4">
+                    <div class="row d-flex justify-content-between align-items-center">
+                      <div class="col-md-2 col-lg-2 col-xl-2">
+                        @if($item->product->images)
+                          @php
+                            $images = json_decode($item->product->images, true); // Decodifica el JSON
+                          @endphp
+                          @if(!empty($images) && is_array($images))
+                            <img src="{{ $images[0] }}" class="img-fluid rounded-3" alt="{{ $item->product->name }}">
+                          @endif
+                        @endif
+                      </div>
+                      <div class="col-md-3 col-lg-3 col-xl-3">
+                        <p class="lead fw-normal mb-2">{{ $item->product->name }}</p>
+                      </div>
+                      <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                        <p>{{ $item->product->description }}</p>
+                      </div>
+                      <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                        <h5 class="mb-0">L. {{ number_format($item->product->price * $item->quantity, 2) }}</h5>
+                        <?php $total += $item->product->price * $item->quantity; ?>
+                      </div>
+                      <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                        <form action="{{ route('cart.remove', $item->id) }}" method="POST">
+                          @csrf
+                          @method('DELETE')
+                          <button type="submit" class="text-danger bg-transparent border-0">
+                            <i class="fas fa-trash fa-lg"></i>
+                          </button>
+                        </form>
+                      </div>
+                    </div>
                   </div>
-                  @endif @endif
-                  <div class="col-md-3 col-lg-3 col-xl-3">
-                    <p class="lead fw-normal mb-2">{{ $item->product->name }}</p>
-                    <p>
+                </div>
+              @endforeach
 
-                    </p>
+              <div class="card mb-4">
+                <div class="card-body p-4 d-flex flex-row">
+                  <div data-mdb-input-init class="form-outline flex-fill align-items-center d-flex">
+                    <input type="text" id="form1" class="form-control form-control-lg" placeholder="Código de descuento" />
+                    <label class="form-label" for="form1"></label>
                   </div>
-                  <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                    <p>{{$item->product->description}} </p>
-                  </div>
-                  <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                    <h5 class="mb-0">L. {{ number_format($item->product->price * $item->quantity, 2) }}</h5>
-                    <?php
-                    $total += $item->product->price * $item->quantity;
-                    ?>
-                  </div>
-                  <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                    <form action="{{ route('cart.remove', $item->id) }}" method="POST">
-                      @csrf
-                      @method('DELETE')
-                      <button type="submit" class="text-danger bg-transparent border-0">
-                        <i class="fas fa-trash fa-lg"></i>
-                      </button>
-                    </form>
+                  <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-warning btn-lg ms-3">Aplicar</button>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-body d-flex justify-content-between">
+                  <a href="{{ route('cart.payment') }}" class="btn btn-success btn-block btn-lg">Proceder al pago</a>
+                  <div>
+                    <p class="fw-bold texto">Total: </p>
+                    <span>L. {{ number_format($total, 2) }}</span>
                   </div>
                 </div>
               </div>
-            </div>
-            @endforeach
-
-            <div class="card mb-4">
-              <div class="card-body p-4 d-flex flex-row">
-                <div data-mdb-input-init class="form-outline flex-fill align-items-center d-flex">
-                  <input type="text" id="form1" class="form-control form-control-lg"
-                    placeholder="Código de descuento" />
-                  <label class="form-label" for="form1"></label>
-                </div>
-                <button type="button" data-mdb-button-init data-mdb-ripple-init
-                  class="btn btn-outline-warning btn-lg ms-3">Aplicar</button>
-              </div>
-            </div>
-
-            <div class="card">
-              <div class="card-body d-flex justify-content-between">
-                <button type="button" data-mdb-button-init data-mdb-ripple-init
-                  class="btn btn-success btn-block btn-lg">Proceder al pago</button>
-                <div>
-                  <p class="fw-bold texto">Total: </p>
-                  <span>L. {{ number_format($total, 2) }}</span>
-                </div>
-              </div>
-            </div>
+            @else
+              <p>Tu carrito está vacío.</p>
+            @endif
           </div>
-          @else
-          <p>Tu carrito está vacío.</p>
-          @endif
         </div>
-      </div>
       </div>
     </section>
   </main>
